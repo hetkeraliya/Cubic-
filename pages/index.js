e58badd8e07b41a1921e31b7a5201c5b6fe7f062
Het Keraliya <jalpa5398@gmail.com>
2026-04-16 16:46:22 +0530

    Add TournamentLogin component to index page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// pages/index.js  (or app/page.jsx if you use App Router)
+//
+// If using App Router (Next.js 13+), add this at the very top:
+// "use client";
+
+import TournamentLogin from "../components/tournament-login";
+
+export default function Home() {
+  return <TournamentLogin />;
+    }
